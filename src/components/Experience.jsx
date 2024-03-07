8454c0cd03898ebbbe859d6e7622d1db03b822f4
Daniela Homobono <danielahomobono81@gmail.com>
2024-03-07 09:21:34 -0300

    componentes de menu,interface y scroll

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,13 +1,32 @@
-import { OrbitControls } from "@react-three/drei";
+import { Environment, OrbitControls, Text } from "@react-three/drei";
+import { Avatar } from "./Avatar";
+import { useControls } from "leva";
+
 
 export const Experience = () => {
+
+const {animation} = useControls({
+  animation: {
+  value: "Hello",
+  options: ["Hello", "Falling", "Standing"]
+},
+})
+
+
   return (
     <>
       <OrbitControls />
-      <mesh>
-        <boxGeometry />
-        <meshNormalMaterial />
-      </mesh>
+      
+      <Environment preset="sunset" />
+      <group position-y={-1}>
+      <Avatar animation={animation} />
+      {/*<Text font={"fonts/Poppins-Black.ttf"} position-x={-1.3} position-y={-0.5} position-z={1} lineHeight={0.8} textAlign="center">
+        MY LITTLE{"\n"} CAMPING
+        <meshBasicMaterial color="white" />
+  </Text>*/}
+  <ambientLight scale={1} />
+  </group>
+  
     </>
   );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import { Experience } from './components/Experience'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+    
   </React.StrictMode>,
 )
